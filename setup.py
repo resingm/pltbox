a750e58b7aae4879627b79ed8746d46e4e658c4d
Max Resing <github@maxresing.de>
2024-06-25 10:49:15 +0200

    Added building requirements

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.1.0"
+
+setup (
+    name="pltbox",
+    version=__version__,
+    description="A toolbox for plotting stuff",
+    author="Max Resing",
+    author_email="pypi.org@maxresing.de",
+    url="https://github.com/resingm/pltbox",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib >= 3.8",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
+
