102f1a8d2a068ee4079dd620ebdfef331cbb8494
Max Resing <github@maxresing.de>
2024-10-15 12:45:30 +0200

    Added an initialization function|

--- pltbox/plots.py
+++ pltbox/plots.py
@@ -3,6 +3,32 @@ from matplotlib import pyplot as plt
 
 from pltbox import colors
 
+initialized = False
+paper_mode = False
+bg = None
+fg = None
+
+def init(is_paper: bool = False):
+    global bg, fg, paper_mode, initialized
+    
+    initialized = True
+    bg_color: str = "#202020"
+    fg_color: str = "#ffffff"
+    paper_mode = is_paper
+    
+    bg = colors.n_shades_of(bg_color, 2)
+    fg = colors.n_shades_of(fg_color, 2)
+
+    # Set the default color for text in all figures
+    mpl.rcParams["text.color"] = fg[0]
+    mpl.rcParams["axes.edgecolor"] = fg[1]
+    mpl.rcParams["axes.labelcolor"] = fg[1]
+    mpl.rcParams["xtick.color"] = fg[1]
+    mpl.rcParams["ytick.color"] = fg[1]
+    mpl.rcParams["legend.facecolor"] = bg[1]
+    
+    
+
 def fig(
     rows: int,
     cols: int,
@@ -10,22 +36,15 @@ def fig(
     size_y: float = 4.0,
     sharex: bool = False,
     sharey: bool = False,
-    is_paper: bool = False,
-    bg_color: str = "#202020",
-    fg_color: str = "#ffffff",
 ) -> tuple:
+    global paper_mode, initialized, bg, fg
     
-    if not is_paper:
-        bg = colors.n_shades_of(bg_color, 2)
-        fg = colors.n_shades_of(fg_color, 2)
-
-        # Set the default color for text in all figures
-        mpl.rcParams["text.color"] = fg[0]
-        mpl.rcParams["axes.edgecolor"] = fg[1]
-        mpl.rcParams["axes.labelcolor"] = fg[1]
-        mpl.rcParams["xtick.color"] = fg[1]
-        mpl.rcParams["ytick.color"] = fg[1]
-        mpl.rcParams["legend.facecolor"] = bg[1]
+    if not initialized:
+        raise Exception("Module `plots` not initialized. Call plots.init(is_paper: bool)")
+    
+    if paper_mode:
+        fig, ax = plt.subplots(rows, cols, figsize=(size_x, size_y), sharex=sharex, sharey=sharey)
+    else:
         fig, ax = plt.subplots(
                 rows,
                 cols,
@@ -34,18 +53,16 @@ def fig(
                 sharey=sharey,
                 facecolor=bg[0],
         )
-    else:
-        fig, ax = plt.subplots(rows, cols, figsize=(size_x, size_y), sharex=sharex, sharey=sharey)
 
     if (rows * cols) > 1:
         ax = ax.T.flatten()
-        for e in ax:
-            if not is_paper:
+        
+        if not paper_mode:
+            for e in ax:
                 e.set_facecolor(bg[0])
     else:
-        if not is_paper:
+        if not paper_mode:
             ax.set_facecolor(bg[0])
 
     return fig, ax
-
 
